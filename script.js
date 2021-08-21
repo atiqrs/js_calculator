677af21ab0b63c3274894caf6908b2583566befd
Atiqur Rahman <atiqrs.me@gmail.com>
2021-08-21 12:45:41 +0600

    event listener added

--- script.js
+++ script.js
@@ -6,6 +6,32 @@ const allClearButton = document.querySelector('[data_all_clear]')
 const oldValueTextElement = document.querySelector('[data_old_value]')
 const newValueTextElement = document.querySelector('[data_new_value]')
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
 class Calculator {
     constructor(oldValueTextElement, newValueTextElement) {
         this.oldValueTextElement = oldValueTextElement
